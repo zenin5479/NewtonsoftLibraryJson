c124496185e1433f1ada175c3dc3c4298ce96eeb
zenin5479 <zenin5479@gmail.com>
2023-08-25 21:41:33 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,9 @@
+﻿namespace NewtonsoftLibraryJson
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonsoftLibraryJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                Age = 19,
+                FirstName = "Newtonsoft",
+                LastName = "Men"
+            };
+
+            var jsonDatata = JsonConvert.SerializeObject(person);
+
+            var person2 = JsonConvert.DeserializeObject<Person>(jsonDatata);
+
+            //Console.WriteLine(person2);
+            Console.ReadKey();
+        }
+    }
+}
